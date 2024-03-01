b59aa5f3ee00c975ef18d45f98fff94fac64beeb
devin-saini <160544488+devin-saini@users.noreply.github.com>
2024-03-01 09:27:03 -0600

    Update Sleuth.R

--- Sleuth.R
+++ Sleuth.R
@@ -1,11 +1,22 @@
+#load package
 library (sleuth)
 
+#read table describing samples and kallisto files
 stab = read.table("sleuth_table.txt", header=TRUE)
+
+#make sleuth object
 so = sleuth_prep(stab)
+
+#fit models to compare conditions and perform likelihood ratio test between conditions
 so = sleuth_fit(so, ~condition, 'full')
 so = sleuth_fit(so, ~1, 'reduced')
 so = sleuth_lrt(so, 'reduced', 'full')
 
+#extract results
 sleuth_table = sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
+
+#filter most significant results and sort by pval
 sleuth_significant = dplyr::filter(sleuth_table, qval <= 0.05) |> dplyr::arrange(pval)
+
+#write values to file
 write.table(sleuth_significant, file="sleuth_results.txt",quote = FALSE,row.names = FALSE)
